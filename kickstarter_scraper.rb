38ff117b7d7e0193817041462dd1711de27cb0fd
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-09-10 17:30:19 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -18,16 +18,3 @@ def create_project_hash
   projects
 
 end
-
-
-
-
-
-
-# returns a hash
-#   includes at least five projects
-#   project titles point to a hash of info
-#   each project has an image link hosted on AmazonAWS
-#   each project has a description which is a string
-#   each project has a location which is a string
-#   each project has percentage funded listed which is an integer
